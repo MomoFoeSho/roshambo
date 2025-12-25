6bec4b76ddedb396e6a3d7125c074be3beddc249
MomoFoeSho <mospagats9@gmail.com>
2025-12-25 12:45:55 -0500

    added code for options

---
+++ javascript.js
@@ -0,0 +1,16 @@
+function getComputerChoice() {
+
+
+    if (getComputerChoice <= .3333) {
+        getComputerChoice = "Rock";
+    } else if (getComputerChoice <= .6667) {
+        getComputerChoice = "Paper";
+    } else {
+        getComputerChoice = "Scissors";
+    }   
+
+     
+
+}
+
+console.log(getComputerChoice);
